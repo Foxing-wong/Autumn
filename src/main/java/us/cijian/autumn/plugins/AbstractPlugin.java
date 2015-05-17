6ddfce3d4eb379635ab919d2c48ccc972d01cca5
MurphyL <im@murphyl.com>
2015-05-17 11:08:03 +0800

    应用插件的概念

--- src/main/java/us/cijian/autumn/plugins/AbstractPlugin.java
+++ src/main/java/us/cijian/autumn/plugins/AbstractPlugin.java
@@ -4,10 +4,15 @@ import us.cijian.autumn.pojo.Message;
 
 /**
  * Created by MurphyL on 2015/5/17.
- * 不知道有什么用，既然写了，就当作规范留着吧
  */
 public abstract class AbstractPlugin {
 
+    /**
+     * 每个插件的唯一指定入口方法
+     * @param message
+     * @param <T>
+     * @return
+     */
     public abstract <T extends Message> T call(T message);
 
 }
